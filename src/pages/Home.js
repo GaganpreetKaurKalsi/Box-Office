e19003d7ee186fe2328907c9111f3ead4877f75a
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2021-07-01 13:25:09 +0530

    added api and search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-    return (
-      <div>
-        <MainPageLayout> This is Home Page</MainPageLayout>
-      </div>
-    );
-}
+    const [input, setInput] = useState('');
 
-export default Home
+    const onInputChange = e => {
+        setInput(e.target.value);
+    };
+    const onSearch = () => {
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(res => res.json()).then((result) => { console.log(result) })
+    };
+    const onKeyDown = (e) => {
+        if (e.key === "Enter") {
+            onSearch()
+        }
+    }
+    return (
+        <div>
+            <MainPageLayout>
+                <input type="text" onChange={onInputChange} onKeyDown={onKeyDown}value={input} />
+                <button type="button" onClick={onSearch} >
+                    Search
+                </button>
+            </MainPageLayout>
+        </div>
+    );
+};
+
+export default Home;
