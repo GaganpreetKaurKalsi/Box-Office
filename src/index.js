652ee95af7aaf6c6391bca81a0fb82a1ca8adebe
Gaganpreet Kaur Kalsi <54144759+GaganpreetKaurKalsi@users.noreply.github.com>
2021-07-04 16:34:33 +0530

    changed from BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
